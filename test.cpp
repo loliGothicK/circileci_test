33b7bafe54bd93c3135275ae8885da83d2928836
Yuki Wada <loligothick@gmail.com>
2017-10-27 10:57:17 +0900

    hoge

--- test.cpp
+++ test.cpp
@@ -1,12 +1,13 @@
 #include <type_traits>
+#include <tuple>
 #include <iostream>
 
-template < class T >
-auto test(T&&) -> std::enable_if_t<std::is_integral<std::decay_t<T>>::value>
+template < class Tuple >
+auto test(Tuple&&) -> std::enable_if_t<std::is_integral<std::tuple_element_t<0,std::decay_t<Tuple>>>::value>
 {
   std::cout << "ok" << std::endl;
 }
 
 int main(){
-  test(1);
+  test(std::make_tuple(1,2,3,4));
 }
